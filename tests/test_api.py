08bcf54b93ebdfc261c3a028c49c838649b29541
Lyetenth <20485546+Lyetenth@users.noreply.github.com>
2020-07-02 11:14:10 -0400

    Fix file test

--- tests/test_api.py
+++ tests/test_api.py
@@ -25,7 +25,9 @@ class TestApi:
     def test_valid_call(self):
         api_key = os.environ['FSF_API_KEY']
         fs = firststreet.FirstStreet(api_key)
-        fs.adaptation.get_detail([29], csv=False, limit=100)
+        adap = fs.adaptation.get_detail([29], csv=False, limit=100)
+        assert len(adap) == 1
+        assert adap[0].name == 'Riverfront Park'
 
     def test_invalid_call(self):
         api_key = os.environ['FSF_API_KEY']
@@ -35,18 +37,39 @@ class TestApi:
 
     def test_file(self):
         with open(os.getcwd() + "/" + "sample.txt", "w+") as file:
-            file.write("395133768")
-            file.write("10212 BUCKEYE RD, Cleveland, Ohio")
-            file.write("(41.48195701269418, -81.6138601319609)")
+            file.write("395133768\n")
+            file.write("10212 BUCKEYE RD, Cleveland, Ohio\n")
+            file.write("(41.48195701269418, -81.6138601319609)\n")
 
         api_key = os.environ['FSF_API_KEY']
         fs = firststreet.FirstStreet(api_key)
-        fs.location.get_detail("sample.txt", "property", csv=True)
+        loc = fs.location.get_detail("sample.txt", "property", csv=True)
+        assert len(loc) == 3
+        assert loc[0].route == 'BUCKEYE RD'
+        assert loc[1].route == 'BUCKEYE RD'
+        assert loc[2].route == 'BUCKEYE RD'
+
+        os.remove(os.getcwd() + "/" + "sample.txt")
+
+    def test_invalid_file(self):
+        with open(os.getcwd() + "/" + "sample.txt", "w+") as file:
+            file.write("395133768, 10212 BUCKEYE RD, Cleveland, Ohio, (41.48195701269418, -81.6138601319609)")
+
+        api_key = os.environ['FSF_API_KEY']
+        fs = firststreet.FirstStreet(api_key)
+        loc = fs.location.get_detail("sample.txt", "property", csv=True)
+        assert len(loc) == 1
+
+        os.remove(os.getcwd() + "/" + "sample.txt")
 
     def test_multi_type(self):
         api_key = os.environ['FSF_API_KEY']
         fs = firststreet.FirstStreet(api_key)
-        fs.location.get_detail([395133768,
-                                "10212 BUCKEYE RD, Cleveland, Ohio",
-                                (41.48195701269418, -81.6138601319609)],
-                               "property", csv=True)
+        loc = fs.location.get_detail([395133768,
+                                      "10212 BUCKEYE RD, Cleveland, Ohio",
+                                      (41.48195701269418, -81.6138601319609)],
+                                     "property", csv=True)
+        assert len(loc) == 3
+        assert loc[0].route == 'BUCKEYE RD'
+        assert loc[1].route == 'BUCKEYE RD'
+        assert loc[2].route == 'BUCKEYE RD'
